d755797210fa8ee3942d5b0992dae72497719a89
akanimo sam okure <akan.okure@gmail.com>
2026-05-10 22:39:34 +0100

    create ICardsService and Implement

---
+++ src/main/java/mantletechsolutions/cards/service/impl/CardsServiceImpl.java
@@ -0,0 +1,27 @@
+package mantletechsolutions.cards.service.impl;
+
+import mantletechsolutions.cards.dto.CardsDto;
+import mantletechsolutions.cards.service.ICardsService;
+
+public class CardsServiceImpl implements ICardsService {
+
+    @Override
+    public void createCard(String mobileNumber) {
+
+    }
+
+    @Override
+    public CardsDto fetchCard(String mobileNumber) {
+        return null;
+    }
+
+    @Override
+    public boolean updateCard(CardsDto cardsDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCard(String mobileNumber) {
+        return false;
+    }
+}
