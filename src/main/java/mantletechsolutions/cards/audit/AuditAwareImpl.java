89785345f592000b7b3cdfd1bcbfade4f280fea8
akanimo sam okure <akan.okure@gmail.com>
2026-05-12 21:53:45 +0100

    add component annotation to register audit bean

--- src/main/java/mantletechsolutions/cards/audit/AuditAwareImpl.java
+++ src/main/java/mantletechsolutions/cards/audit/AuditAwareImpl.java
@@ -1,9 +1,10 @@
 package mantletechsolutions.cards.audit;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
-
+@Component("auditAwareImpl")
 public class AuditAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
